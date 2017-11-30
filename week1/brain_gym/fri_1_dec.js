2f4f97f5d5145888a964f4f078e08dab5597573f
jwon114 <jthgwong@gmail.com>
2017-12-01 09:40:15 +1100

    fri_1_dec

---
+++ week1/brain_gym/fri_1_dec.js
@@ -0,0 +1,49 @@
+// 1. Print out all the leap years in the last 100 years.
+
+var date = new Date();
+var currentYear = date.getFullYear();
+var firstYear = currentYear - 100;
+var lastLeapYear = 2016;
+var leapYears = [];
+
+for (var year = lastLeapYear; year >= firstYear; year-=4) {
+	leapYears.push(year);
+}
+
+console.log(leapYears);
+
+
+// 2. For numbers between 0 and 200:
+// 	a) print out multiples of 7.
+// 	b) print out every second odd number.
+// 	c) print out all prime numbers.
+
+var multiplesOfSeven = []
+for (var i = 0; i <= 200; i = i + 7) {
+	multiplesOfSeven.push(i);
+};
+
+console.log(multiplesOfSeven);
+
+var everySecondOddNum = [];
+for (var i = 1; i <= 200; i+=4) {
+	everySecondOddNum.push(i);
+};
+
+console.log(everySecondOddNum);
+
+
+var primeNum = [];
+for (var i = 0; i <= 200; i++) {
+	var prime = true;
+	if (i === 0 || i === 1) { prime = false }
+	for (var num = 2; num < i; num++) {
+		if (i % num === 0) {
+			prime = false;
+			break;
+		}
+	}
+	if (prime) { primeNum.push(i) }
+};
+
+console.log(primeNum);
